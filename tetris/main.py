87d73dc21cc09b144cb4eb8f6fd6591d4dc3d4f8
Hrystyna Yaskulska <yaskulskahrystyna@gmail.com>
2024-12-10 23:23:21 +0200

    Add back button for tetris game

--- tetris/main.py
+++ tetris/main.py
@@ -1,3 +1,4 @@
+import subprocess
 import pygame,sys
 from game import Game
 from colors import Colors
@@ -5,11 +6,17 @@ from colors import Colors
 pygame.init()
 
 title_font = pygame.font.Font(None, 40)
+button_font = pygame.font.Font(None, 30)
+info_font = pygame.font.Font(None, 20)
+
+back_button_surface = button_font.render("Back", True, Colors.white)
 score_surface = title_font.render("Score", True, Colors.white)
 next_surface = title_font.render("Next", True, Colors.white)
 game_over_surface = title_font.render("GAME OVER", True, Colors.white)
+restart_info_surface = info_font.render("(press any key to restart)", True, Colors.white)
 
 
+back_button_rect = pygame.Rect(420, 575, 70, 35)
 score_rect = pygame.Rect(320, 55, 170, 60)
 next_rect = pygame.Rect(320, 215, 170, 180)
 
@@ -26,8 +33,14 @@ pygame.time.set_timer(GAME_UPDATE, 200)
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            subprocess.Popen(["python", "main_menu.py"])
             pygame.quit()
             sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if back_button_rect.collidepoint(event.pos):
+                subprocess.Popen(["python", "main_menu.py"])
+                pygame.quit()
+                sys.exit()
         if event.type == pygame.KEYDOWN:
             if game.game_over == True:
                 game.game_over = False
@@ -53,10 +66,17 @@ while True:
 
     if game.game_over == True:
         screen.blit(game_over_surface, (320, 450, 50, 50))
+        screen.blit(restart_info_surface, (328, 480, 50, 50))
+
     pygame.draw.rect(screen, Colors.light_violet, score_rect, 0, 10)
     screen.blit(score_value_surface, score_value_surface.get_rect(centerx = score_rect.centerx, centery = score_rect.centery))
     pygame.draw.rect(screen, Colors.light_violet, next_rect, 0, 10)
     game.draw(screen)
+
+    
+    pygame.draw.rect(screen, Colors.light_violet, back_button_rect, 0, 10)
+    screen.blit(back_button_surface, (back_button_rect.x + 10, back_button_rect.y + 8))
+
 
     pygame.display.update()
     clock.tick(60)
