bf35e3ec5c78f2b6d307b837a10acb0581ac9478
Hrystyna Yaskulska <yaskulskahrystyna@gmail.com>
2024-12-10 21:28:00 +0200

    Add checking for completed rows

--- tetris/grid.py
+++ tetris/grid.py
@@ -24,6 +24,31 @@ class Grid:
         if self.grid[row][column] == 0:
             return True
         return False
+    
+    def is_row_full(self, row):
+        for column in range(self.num_cols):
+            if self.grid[row][column] == 0:
+                return False
+        return True
+    
+    def clear_row(self, row):
+        for column in range(self.num_cols):
+            self.grid[row][column] = 0
+
+    def move_row_down(self, row, num_rows):
+        for column in range(self.num_cols):
+            self.grid[row + num_rows][column] = self.grid[row][column]
+            self.grid[row][column] = 0
+
+    def clear_full_rows(self):
+        completed = 0
+        for row in range(self.num_rows - 1, 0, -1):
+            if self.is_row_full(row):
+                self.clear_row(row)
+                completed += 1
+            elif completed > 0:
+                self.move_row_down(row, completed)
+        return completed
 
     def draw(self, screen):
         for row in range(self.num_rows):
